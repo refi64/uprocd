2edd0d4b3c7868ebb427ae3ab22b8aa8b99dedcf
Ryan Gonzalez <rymg19@gmail.com>
2018-01-17 16:15:19 -0600

    Finish up docs

--- api/uprocd.h
+++ api/uprocd.h
@@ -7,9 +7,15 @@
 
 #define UPROCD_EXPORT __attribute__((visibility("default")))
 
+typedef void (*uprocd_module_entry_type)();
+UPROCD_EXPORT void uprocd_module_entry();
+
 UPROCD_EXPORT const char * uprocd_module_directory();
 UPROCD_EXPORT char * uprocd_module_path(const char *path);
 UPROCD_EXPORT void uprocd_module_path_free(char *path);
+
+typedef void (*uprocd_exit_handler)(void *userdata);
+UPROCD_EXPORT void uprocd_on_exit(uprocd_exit_handler func, void *userdata);
 
 UPROCD_EXPORT int uprocd_config_present(const char *key);
 UPROCD_EXPORT int uprocd_config_list_size(const char *key);
@@ -20,19 +26,13 @@ UPROCD_EXPORT const char * uprocd_config_string_at(const char *list, int index);
 
 typedef struct uprocd_context uprocd_context;
 
-UPROCD_EXPORT void uprocd_context_get_args(uprocd_context *ctx, int *pargc,
-                                           char ***pargv);
-UPROCD_EXPORT const char ** uprocd_context_get_env(uprocd_context *ctx);
-UPROCD_EXPORT const char * uprocd_context_get_cwd(uprocd_context *ctx);
+UPROCD_EXPORT uprocd_context * uprocd_run();
+UPROCD_EXPORT void uprocd_context_enter(uprocd_context *ctx);
 UPROCD_EXPORT void uprocd_context_free(uprocd_context *ctx);
 
-UPROCD_EXPORT void uprocd_context_enter(uprocd_context *ctx);
-
-typedef void (*uprocd_exit_handler)(void *userdata);
-UPROCD_EXPORT void uprocd_on_exit(uprocd_exit_handler func, void *userdata);
-UPROCD_EXPORT uprocd_context * uprocd_run();
-
-typedef void (*uprocd_module_entry_type)();
-UPROCD_EXPORT void uprocd_module_entry();
+UPROCD_EXPORT void uprocd_context_get_args(uprocd_context *ctx, int *pargc,
+                                           char ***pargv);
+UPROCD_EXPORT const char * uprocd_context_get_cwd(uprocd_context *ctx);
+UPROCD_EXPORT const char ** uprocd_context_get_env(uprocd_context *ctx);
 
 #endif
