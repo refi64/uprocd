226196474311192bd4afbbf678d7d26aec95e574
Ryan Gonzalez <rymg19@gmail.com>
2018-01-21 18:49:55 -0600

    Whoops...

--- src/uprocd/main.c
+++ src/uprocd/main.c
@@ -36,7 +36,7 @@ config *load_config(const char *module, sds *module_dir) {
   sds xdg_config_home = get_xdg_config_home();
 
   static const char *search_paths[] = {
-    "build/modules",
+    /* "build/modules", */
     "/usr/share/uprocd/modules",
     "/usr/local/share/uprocd/modules",
     "@/uprocd/modules",
